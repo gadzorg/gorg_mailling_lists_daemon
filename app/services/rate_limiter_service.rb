b717d780c61958002b07ed906580d68d226fcc52
Alexandre Narbonne <alexandre.narbonne@gadz.org>
2016-10-06 12:02:29 +0200

    fix types

--- app/services/rate_limiter_service.rb
+++ app/services/rate_limiter_service.rb
@@ -1,8 +1,8 @@
 class RateLimiterService
 
   def initialize(key=GorgMaillingListsDaemon.config['admin_user_id'],time_window=GorgMaillingListsDaemon.config['google_api_rate_time_window'].to_i,max=GorgMaillingListsDaemon.config['google_api_rate_limit'].to_i)
-    @time_window=time_window
-    @max=max
+    @time_window=time_window.to_i
+    @max=max.to_i
     @key="#{key}_#{@time_window}_#{@max}"
     @r=Redis.new(url:GorgMaillingListsDaemon.config['redis_url'])
   end
@@ -25,7 +25,7 @@ class RateLimiterService
   end
 
   def incr(n=1)
-    ret=@r.incrby(@key, n)
+    ret=@r.incrby(@key, n).to_i
     @r.expire(@key, @time_window) if @r.ttl(@key).to_i < 0
     ret
   end
